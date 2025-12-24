2f5d928860a95eab6e527b46f344e7f1cb855674
jongwoo <izowooi@hotmail.com>
2025-12-24 18:37:14 +0900

    qrgenerator 추가

---
+++ qrgenerator/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "WiFi QR Generator",
+  description: "WiFi 정보를 입력하면 QR 코드를 생성하여 손쉽게 WiFi에 연결할 수 있습니다",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
